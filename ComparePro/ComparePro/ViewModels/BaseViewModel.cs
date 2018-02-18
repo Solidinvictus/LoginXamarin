7b180271fbcc2aee93b83301de5a55f2b714024b
Ali-PC\Ali <alismili27@gmail.com>
2018-02-18 14:20:29 +0100

    Instalación del Nugget MVVM LightLibs para nuestro proyecto--> http://www.mvvmlight.net/  para más info. Validación de datos con bindings a objetos creados en las clases ViewModels correspondientes. Sacar mensages de alerta descriptivos para el usuario. Limpiar el campo password al introducir contraseña erronea. Implementacion de la interface INotifyPropertyChanged para refrescar los campos y las instrucciones en fase de ejecucion.

---
+++ ComparePro/ComparePro/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparePro.ViewModels
+{
+    //Clase genérica para evitar la redundancia y duplicidad en neuestro código --Pendiente por implementar
+    public class BaseViewModel
+    {
+
+    }
+}
--- ComparePro/ComparePro/ViewModels/LoginViewModel.cs
+++ ComparePro/ComparePro/ViewModels/LoginViewModel.cs
@@ -5,14 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+
 namespace ComparePro.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
+    //using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    class LoginViewModel
+    class LoginViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;       //Esta interfaz es para refrescar 
+        #endregion
+
+        //Los atributos privados que necesitamos refrescar
+        #region Atributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        private bool isRemembered;
+        #endregion
+
         #region Properties
-        public string Login
+        public string Email
         {
             get;
             set;
@@ -20,28 +38,126 @@ namespace ComparePro.ViewModels
 
         public string Password
         {
-            get;
-            set;
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if(this.password != value)
+                {
+                    this.password = value;
+                    //Para refrescar las instrucciones en tpo de ejecucion
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+            }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
         }
 
         public bool IsRemembered
         {
-            get;
-            set;
+            get
+            {
+                return this.isRemembered;
+            }
+            set
+            {
+                if (this.isRemembered != value)
+                {
+                    this.isRemembered = value;
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(this.IsRemembered)));
+                }
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                if (this.isEnabled != value)
+                {
+                    this.isEnabled = value;
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+                }
+            }
         }
         #endregion
 
         #region Commands
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Login);     //De la libreria instalada GalaSoft.MvvmLight.Command, y le pasamos un metodo por param
+            }
+        }
+
+        //Param del RelayCommand
+        private async void Login()      //Para manejar los alerts en dif dispos, hace falta metodos asincronos
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",        //Titulo del error
+                    "You must enter an email ",     //Mensaje del error
+                    "Accept");      //Texto del boton
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password ",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+            //Esto es temporal, ya que esto debería ir a un archivo de recursos (solucion temporal) y luego en la BBDD
+            if (this.Email != "welltec@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "Error",
+                   "Email or password incorrect ",
+                   "Accept");
+                //Limpiamos el campo del password al ingresar uno erroneo
+                this.Password = string.Empty;       //No va a funcionar pq no está referenciado en el ViewModel-->sol: Implementar la interfaz INotifyPropertyChanged
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                "Ok",
+                "Hell Yeah!!",
+                "Accept");
+            return;
         }
 
         public ICommand RegisterCommand
@@ -55,8 +171,8 @@ namespace ComparePro.ViewModels
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
-
         #endregion  
     }
 }
